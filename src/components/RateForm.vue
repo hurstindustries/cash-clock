a591d372b7a8408132f612830420451cc1ca4898
Joshua Hurst <hurstindustries@gmail.com>
2020-01-27 21:32:07 -0800

    change hourlyRate in RateForm to type text input

--- src/components/RateForm.vue
+++ src/components/RateForm.vue
@@ -2,36 +2,54 @@
   <div class="w-full h-full bg-white fixed pin z-50 overflow-auto bg-smoke-light flex text-left">
     <div class="mx-auto px-8 pt-6 pb-8 mb-4">
       <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="hourlyRate">
-          How much do you make per hour?
-        </label>
-        <input v-model="hourlyRate" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="hourlyRate" type="tel">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="hourlyRate"
+        >How much do you make per hour?</label>
+        <input
+          v-model="hourlyRate"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="hourlyRate"
+          type="text"
+        />
       </div>
       <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="hoursPerday">
-          How many hours do you work per day?
-        </label>
-        <input v-model="hoursPerday" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="hoursPerday" type="text">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="hoursPerday"
+        >How many hours do you work per day?</label>
+        <input
+          v-model="hoursPerday"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="hoursPerday"
+          type="text"
+        />
       </div>
-      <button v-if="!active" name="button" class="w-full bg-green-500 hover:bg-green-700 text-white font-bold mt-4 py-4 px-20 rounded text-lg" @click="submit" v-bind:disabled="active">Save</button>
+      <button
+        v-if="!active"
+        name="button"
+        class="w-full bg-green-500 hover:bg-green-700 text-white font-bold mt-4 py-4 px-20 rounded text-lg"
+        @click="submit"
+        v-bind:disabled="active"
+      >Save</button>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from 'vuex';
 
 export default {
-  name: "RateForm",
-  data(){
-    return { 
+  name: 'RateForm',
+  data() {
+    return {
       hourlyRate: 0,
-      hoursPerday: 8
-    } 
+      hoursPerday: 8,
+    };
   },
   computed: {
-    active () {
-      return this.$store.state.clockRunning
+    active() {
+      return this.$store.state.clockRunning;
     },
   },
   methods: {
@@ -39,7 +57,7 @@ export default {
     submit: function() {
       this.setHourlyRate(parseFloat(this.hourlyRate));
       this.setHoursPerDay(parseFloat(this.hoursPerday));
-    }
+    },
   },
-}
+};
 </script>
